bfd08aacdc2565997e9315eeab540c785268599d
evenhumble <patrickwuke@163.com>
2017-07-04 11:19:18 +0800

    remove ToDOIDGenerator constructor

--- todo-backend-map/src/main/java/io/hedwig/explore/todo/mapimpl/service/ToDoIDGenerator.java
+++ todo-backend-map/src/main/java/io/hedwig/explore/todo/mapimpl/service/ToDoIDGenerator.java
@@ -9,9 +9,6 @@ public final class ToDoIDGenerator {
 
     private static AtomicLong todoId = new AtomicLong();
 
-    private ToDoIDGenerator() {
-    }
-
     public static Long getId(){
         return todoId.incrementAndGet();
     }
